fead4c806c9984682962d3400367831f2dad73d9
ProcoKim <dudgp2030@gmail.com>
2018-09-11 10:37:22 +0900

    Making TestHandler

---
+++ Sample/src/TestHandler.java
@@ -0,0 +1,21 @@
+/**
+* FileName : TestHandler.java
+* Created  : 2018. 9. 11. 오전 10:36:18
+* Author   : donghun.kim
+* Summary  :
+* Copyright (C) 2018 Formal Works Inc. All rights reserved.
+*
+* 이 문서의 모든 저작권 및 지적 재산권은 (주)포멀웍스에게 있습니다.
+* 이 문서의 어떠한 부분도 허가 없이 복제 또는 수정 하거나, 전송할 수 없습니다.
+*/
+
+/**
+ * @author donghun.kim
+ */
+public class TestHandler {
+	private static final String msg = "Hello world!";
+
+	public static void PrintTest() {
+		System.out.println(msg);
+	}
+}
